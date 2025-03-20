1aa358f4fd46e39300f9aab3da09958e198a36f2
Misbah Ur Rehman <21-cs-24@students.uettaxila.edu.pk>
2025-03-21 02:16:52 +0500

    some changes

---
+++ lib/Model.dart
@@ -0,0 +1,15 @@
+class Task {
+  final String id;
+  String title;
+  String description;
+  bool isCompleted;
+  final DateTime dateAdded;
+
+  Task({
+    required this.id,
+    required this.title,
+    required this.description,
+    this.isCompleted = false,
+    required this.dateAdded,
+  });
+}
